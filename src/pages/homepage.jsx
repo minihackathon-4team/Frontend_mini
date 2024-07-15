66b34176edd4f832be37082687b75e4736578c6e
igbinni143 <bin143@naver.com>
2024-07-15 19:42:32 +0900

    eunbin

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Nav from '../components/nav'
 import Moviepage from './moviepage'
+import Mmo from './mmo'
 
 const Homepage = () => {
   return (
     <>
       <Nav/>
-      <Moviepage/>
+      <Mmo/>
     </>
   )
 }
