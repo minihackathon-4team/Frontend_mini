e30ca3123a8a6ac144cd251744c8dd33007f2544
wh0land <cliee0730@gamil.com>
2024-07-15 17:02:05 +0900

    delete useless component

--- src/pages/detailedpage.jsx
+++ src/pages/detailedpage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import Comments from '../components/comments';
 import { useParams } from 'react-router-dom';
 
 export default function Detailedpage() {
